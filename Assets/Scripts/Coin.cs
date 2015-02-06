31e1f66da09c3d32606de482bceb190fd31e99d4
Hyder SM <hyder.mashrour@autodesk.com>
2015-02-06 15:36:39 -0500

    bunch of additions

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour 
+{
+	private CoinCounter coinCounter;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		coinCounter = GameObject.Find("coinText").GetComponent<CoinCounter>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			coinCounter.setCointCount(coinCounter.getCoinCount() + 1);
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+
+public class CoinCounter : MonoBehaviour 
+{
+	private int coinCount = 0;
+	
+	// Update is called once per frame
+	void Start () 
+	{
+		guiText.text = "x" + coinCount;
+	}
+
+	public int getCoinCount(){
+		return coinCount;
+	}
+
+	public void setCointCount(int num){
+		coinCount = num;
+		guiText.text = "x" + coinCount;
+	}
+}
